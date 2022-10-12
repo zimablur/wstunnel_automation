6f653552db196cd0aca85a65ee70161f60ef3828
zimablur <zimablur@proton.me>
2022-10-12 17:00:36 +0300

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/env bash
+
+curl -L -o wstunnel_automation.zip https://github.com/zimablur/wstunnel_automation/archive/refs/heads/master.zip
+unzip wstunnel_automation.zip
+cd wstunnel_automation-master
+./install_online

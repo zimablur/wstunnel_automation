f7594b231625d969c7c682ca866dfac9dd9eeaa8
zimablur <zimablur@proton.me>
2022-11-27 16:48:24 +0200

    Redirect output to file as well

--- connect.sh
+++ connect.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
-echo "Serving ssh server on port 2222 and socks server on port 1080"
+result_dir=$(pwd)/commands_output
+
+echo "Serving ssh server on port 2222 and socks server on port 1080" >> $result_dir/stdout
 cd ~/node_modules/.bin
-./wstunnel -c -t 1080:socksserver:1080 wss://$1:443
+./wstunnel -c -t 1080:socksserver:1080 wss://$1:443 2>&1 | tee $result_dir/stdout
